312a1c0ef26f368bf22905f4c18d9c592899e919
Jonathan Riquer <jonriquer@gmail.com>
2019-08-07 06:16:05 -0600

    constructing state in Chart component

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -2,10 +2,35 @@ import React, {Component} from 'react';
 import {Bar, Line, Pie, Doughnut} from 'react-chartjs-2';
 
 class Chart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData: {
+        labels: ["Miami","Daytona","Orlando"],
+        datasets: [
+          {
+            label: "Population",
+            data: [
+              617594,
+              181045,
+              153060,
+              105519,
+              95072
+            ],
+            backgroundColor: [
+              'rgba(155,145,166,.3)'
+            ]
+          }
+        ]
+      }
+    }
+  }
   render(){
     return(
       <div className='chartCont'>
-        CHART COMPONENT
+        <Bar 
+          data={this.state.chartData}
+        />
       </div>
     )
   }
